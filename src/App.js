732ae3b5698d9e96320295ce0b56dc1d9c1d2b4b
Alberto <alberto.gespi@gmail.com>
2020-02-07 12:20:50 +0100

    AGE

--- src/App.js
+++ src/App.js
@@ -8,14 +8,16 @@ import { AuthProvider } from "./context/authContext";
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-      </Switch>
+      <AuthProvider>
+        <Switch>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
