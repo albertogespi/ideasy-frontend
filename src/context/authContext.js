c5315dfd83ab4c312e72e7d0840c78cf54d8b0d4
Alberto <alberto.gespi@gmail.com>
2020-02-12 19:47:40 +0100

    AGE

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import jwt_decode from "jwt-decode";
 
 const AuthContext = React.createContext();
@@ -6,20 +6,20 @@ const AuthContext = React.createContext();
 const storedUser = JSON.parse(localStorage.getItem("currentUser"));
 
 export function AuthProvider({ children }) {
-	const [isAuth, setIsAuth] = useState(storedUser !== null);
-	const [jwt, setJwt] = useState();
-	const [currentUser, setCurrentUser] = useState(storedUser);
+  const [isAuth, setIsAuth] = useState(storedUser !== null);
+  const [jwt, setJwt] = useState(jwt_decode(storedUser.accessToken));
+  const [currentUser, setCurrentUser] = useState(storedUser);
 
-	return (
-		<AuthContext.Provider
-			value={{ isAuth, setIsAuth, jwt, setJwt, currentUser, setCurrentUser }}
-		>
-			{children}
-		</AuthContext.Provider>
-	);
+  return (
+    <AuthContext.Provider
+      value={{ isAuth, setIsAuth, jwt, setJwt, currentUser, setCurrentUser }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export function useAuth() {
-	const context = useContext(AuthContext);
-	return context;
+  const context = useContext(AuthContext);
+  return context;
 }
