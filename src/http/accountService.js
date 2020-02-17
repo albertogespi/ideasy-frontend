7146244f84dc0c0aaa5bda3c42a58c8cc87f7486
Aitana <aitanammtz@gmail.com>
2020-02-17 12:15:50 +0100

    AMM - project.js ya funciona para usuario no registrado

--- src/http/accountService.js
+++ src/http/accountService.js
@@ -5,60 +5,59 @@ const storedUser = JSON.parse(localStorage.getItem("currentUser"));
 let token = (storedUser && storedUser.accessToken) || null;
 
 axios.interceptors.request.use(
-  function(config) {
-    if (
-      token &&
-      !(
-        config.url.includes("/account/login") ||
-        config.url.includes("/account/new")
-      )
-    ) {
-      config.headers["Authorization"] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  function(error) {
-    return Promise.reject(error);
-  }
+	function(config) {
+		if (
+			token &&
+			!(
+				config.url.includes("/account/login") || config.url.includes("/account/new")
+			)
+		) {
+			config.headers["Authorization"] = `Bearer ${token}`;
+		}
+		return config;
+	},
+	function(error) {
+		return Promise.reject(error);
+	},
 );
 
 axios.interceptors.response.use(
-  function(response) {
-    if (response.data.accessToken) {
-      localStorage.setItem("currentUser", JSON.stringify(response.data));
-      token = response.data.accessToken;
-    }
-    return response;
-  },
-  function(error) {
-    if (
-      error.response.status === 401 &&
-      !error.config.url.includes("/account/login")
-    ) {
-      localStorage.removeItem("currentUser");
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  }
+	function(response) {
+		if (response.data.accessToken) {
+			localStorage.setItem("currentUser", JSON.stringify(response.data));
+			token = response.data.accessToken;
+		}
+		return response;
+	},
+	function(error) {
+		if (
+			error.response.status === 401 &&
+			!error.config.url.includes("/account/login")
+		) {
+			localStorage.removeItem("currentUser");
+			window.location.href = "/access";
+		}
+		return Promise.reject(error);
+	},
 );
 
 export function createAccount(accountData) {
-  return axios.post(
-    `${process.env.REACT_APP_BACKEND_URL}/account/new`,
-    accountData
-  );
+	return axios.post(
+		`${process.env.REACT_APP_BACKEND_URL}/account/new`,
+		accountData,
+	);
 }
 
 export function login(accountData) {
-  return axios.post(
-    `${process.env.REACT_APP_BACKEND_URL}/account/login`,
-    accountData
-  );
+	return axios.post(
+		`${process.env.REACT_APP_BACKEND_URL}/account/login`,
+		accountData,
+	);
 }
 
 export function deleteAccount(accountData) {
-  return axios.delete(
-    `${process.env.REACT_APP_BACKEND_URL}/account`,
-    accountData
-  );
+	return axios.delete(
+		`${process.env.REACT_APP_BACKEND_URL}/account`,
+		accountData,
+	);
 }
