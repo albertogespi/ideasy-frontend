3a3083d93cf8829d8dc9220a14ede937f17b5768
Alberto <alberto.gespi@gmail.com>
2020-02-18 02:25:47 +0100

    AGE - cambios diseño User y ProjectList, instalada librería para rating

---
+++ src/components/Rating.js
@@ -0,0 +1,28 @@
+import React from "react";
+import Rating from "@material-ui/lab/Rating";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+
+export function SimpleRating({ readOnly }) {
+  const [value, setValue] = React.useState(0);
+  if (readOnly) {
+    return (
+      <div classname="stars-rating">
+        <Rating name="read-only" value="5" size="large" readOnly />
+        {/* <Typography component="legend">Puntuación media</Typography> */}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <Rating
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+      </div>
+    );
+  }
+}
--- src/pages/User.js
+++ src/pages/User.js
@@ -1,35 +1,58 @@
 import React, { useState, useEffect } from "react";
+
 import { Header } from "../components/Header";
+import { getAvgRatings } from "../http/projectsService";
+import { getContributedProjects } from "../http/projectsService";
 import { getUser } from "../http/userService";
+import { SimpleRating } from "../components/Rating";
 
 export function User() {
   const userId = window.location.href.split("/")[4];
-  console.log(userId);
 
   const [user, setUser] = useState(undefined);
+  const [rating, setRating] = useState(undefined);
+
   useEffect(() => {
     getUser(userId).then(response => {
       setUser(response.data);
     });
+    getAvgRatings(userId).then(response => {
+      setRating(response.data);
+    });
   }, []);
 
-  if (user !== undefined) {
+  if (user !== undefined && rating !== undefined) {
     console.log(user);
     return (
       <main>
         <Header isAccessWindow={false} />
-        <section className="centered-container">
-          <div className="profile-photo">
-            <img src={user.avatarUrl} alt="" name="profile photo"></img>
-          </div>
-          <p>{user.role === "dev" ? "Desarrollador" : "Organización"}</p>
-          <h1 className="profile-name">{user.name}</h1>
-          <div>
-            <h2>Contacto</h2>
-            <p>Email: {user.contactEmail || user.email}</p>
-            {user.contactWeb && <p>Web: {user.contactWeb}</p>}
-          </div>
-        </section>
+        <div className="centered-container">
+          <section className="centered-container" id="user-container">
+            <div className="profile-photo">
+              <img src={user.avatarUrl} alt="" name="profile photo"></img>
+            </div>
+            <h1>{user.name}</h1>
+            <p>{user.role === "dev" ? "Desarrollador" : "Organización"}</p>
+            <div className="centered-container" id="rating">
+              {user.role === "dev" && (
+                <div className="centered-container">
+                  <SimpleRating readOnly={true} id="stars" />
+                  <p>Puntuación media</p>
+                  {rating
+                    ? `${rating} estrellas`
+                    : "Este usuario aún no ha recibido puntuaciones."}
+                </div>
+              )}
+            </div>
+            <div className="centered-container" id="contact">
+              <h2>Contacto</h2>
+              <p>{user.contactEmail || user.email}</p>
+              {user.contactWeb !== "NULL" && (
+                <a href={user.contactWeb}>Enlace web</a>
+              )}
+            </div>
+          </section>
+        </div>
       </main>
     );
   } else {
