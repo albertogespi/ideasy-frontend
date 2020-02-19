4c56dcb842b6f45f02e4537d67b37ac2feb62475
Alberto <alberto.gespi@gmail.com>
2020-02-19 03:34:08 +0100

    AGE - css NewProject, MyProfile y project

--- src/components/DevProject.js
+++ src/components/DevProject.js
@@ -1,54 +1,55 @@
 import React from "react";
 
 export function DevProject({ project }) {
-	return (
-		<section>
-			<section id='top'>
-				<section id='visible-info'>
-					<h1>Título</h1>
-					<p>{project.description}</p>
-				</section>
-				<section id='project-info'>
-					<ul>
-						<li>
-							<img
-								src='https://img.icons8.com/android/24/000000/sun.png'
-								alt=''
-								id='supersmall-icon'
-							/>
-							<p>{project.created_at.split("T")[0]}</p>
-						</li>
-						<li>
-							<img
-								src='https://img.icons8.com/material-outlined/24/000000/user--v1.png'
-								alt=''
-								id='supersmall-icon'
-							/>
-							<p>
-								{project.number_of_followers}
-								{project.number_of_followers === 1 ? " seguidor" : " seguidores"}
-							</p>
-						</li>
-						<li>
-							<p>Categoría: {project.category}</p>
-						</li>
-						<li>
-							<p>
-								Complejidad:
-								{project.complexity === 1
-									? " Fácil"
-									: project.complexity === 2
-									? " Medio"
-									: " Difícil"}
-							</p>
-						</li>
-					</ul>
-				</section>
-			</section>
-			<section id='middle'>
-				<h2>Descripción detallada</h2>
-				<p>{project.details}</p>
-			</section>
-		</section>
-	);
+  return (
+    <section>
+      <section id="top">
+        <section id="visible-info">
+          <h1>{project.title}</h1>
+          <p>{project.description}</p>
+        </section>
+        <section id="dev-project-info">
+          <ul>
+            <li>
+              <img
+                src="https://img.icons8.com/android/24/000000/sun.png"
+                alt=""
+                id="supersmall-icon"
+              />
+              <p>{project.created_at.split("T")[0]}</p>
+            </li>
+            <li>
+              <img
+                src="https://img.icons8.com/material-outlined/24/000000/user--v1.png"
+                alt=""
+                id="supersmall-icon"
+              />
+              <p>
+                {project.number_of_followers}
+                {project.number_of_followers === 1
+                  ? " seguidor"
+                  : " seguidores"}
+              </p>
+            </li>
+            <li>
+              <p>Categoría: {project.category}</p>
+            </li>
+            <li>
+              <p>
+                Complejidad:
+                {project.complexity === 1
+                  ? " Fácil"
+                  : project.complexity === 2
+                  ? " Medio"
+                  : " Difícil"}
+              </p>
+            </li>
+          </ul>
+        </section>
+      </section>
+      <section id="middle">
+        <p>{project.details}</p>
+      </section>
+    </section>
+  );
 }
--- src/pages/Project.js
+++ src/pages/Project.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { useAuth } from "../context/authContext";
 import {
-	getUsersFollowingProject,
-	getProject,
-	followProject,
+  getUsersFollowingProject,
+  getProject,
+  followProject
 } from "../http/projectService";
 import { getDocuments } from "../http/documentsService";
 import { Header } from "../components/Header";
@@ -17,174 +17,176 @@ const OWNER_VIEW = 1;
 const ONLY_READ_VIEW = 0;
 
 export function Project() {
-	const { register } = useForm({
-		mode: "onBlur",
-	});
-	const { isAuth, jwt } = useAuth();
+  const { register } = useForm({
+    mode: "onBlur"
+  });
+  const { isAuth, jwt } = useAuth();
 
-	const projectId = window.location.href.split("/")[4];
+  const projectId = window.location.href.split("/")[4];
 
-	const [isFollower, setIsFollower] = useState();
-	const [myContributions, setMyContributions] = useState([]);
+  const [isFollower, setIsFollower] = useState();
+  const [myContributions, setMyContributions] = useState([]);
 
-	const [project, setProject] = useState(undefined);
-	const [usersFollowing, setUsersFollowing] = useState(undefined);
-	const [documents, setDocuments] = useState(undefined);
+  const [project, setProject] = useState(undefined);
+  const [usersFollowing, setUsersFollowing] = useState(undefined);
+  const [documents, setDocuments] = useState(undefined);
 
-	const [typeOfProfile, setTypeOfProfile] = useState(undefined);
+  const [typeOfProfile, setTypeOfProfile] = useState(undefined);
 
-	useEffect(() => {
-		if (project !== undefined) {
-			if (isAuth) {
-				if (jwt.role === "dev") {
-					setTypeOfProfile(DEVELOPER_VIEW);
-				} else {
-					if (jwt.userId === project.user_id) {
-						setTypeOfProfile(OWNER_VIEW);
-					} else {
-						setTypeOfProfile(ONLY_READ_VIEW); // 0 = not registered person or org watching other org projects
-					}
-				}
-			} else {
-				setTypeOfProfile(ONLY_READ_VIEW);
-			}
-		}
-	}, [project]);
+  useEffect(() => {
+    if (project !== undefined) {
+      if (isAuth) {
+        if (jwt.role === "dev") {
+          setTypeOfProfile(DEVELOPER_VIEW);
+        } else {
+          if (jwt.userId === project.user_id) {
+            setTypeOfProfile(OWNER_VIEW);
+          } else {
+            setTypeOfProfile(ONLY_READ_VIEW); // 0 = not registered person or org watching other org projects
+          }
+        }
+      } else {
+        setTypeOfProfile(ONLY_READ_VIEW);
+      }
+    }
+  }, [project]);
 
-	useEffect(() => {
-		getProject(projectId).then((response) => {
-			setProject(response.data);
-		});
+  useEffect(() => {
+    getProject(projectId).then(response => {
+      setProject(response.data);
+    });
 
-		getUsersFollowingProject(projectId).then((response) => {
-			setUsersFollowing(response.data);
-		});
+    getUsersFollowingProject(projectId).then(response => {
+      setUsersFollowing(response.data);
+    });
 
-		getDocuments(projectId).then((response) => {
-			setDocuments(response.data);
-		});
-	}, []);
+    getDocuments(projectId).then(response => {
+      setDocuments(response.data);
+    });
+  }, []);
 
-	useEffect(() => {
-		if (typeOfProfile === 2 && documents !== undefined) {
-			setMyContributions(devContributions());
-		}
-	}, [documents]);
+  useEffect(() => {
+    if (typeOfProfile === 2 && documents !== undefined) {
+      setMyContributions(devContributions());
+    }
+  }, [documents]);
 
-	const handleFollow = () => {
-		followProject(projectId);
-		setIsFollower(true);
-	};
+  const handleFollow = () => {
+    followProject(projectId);
+    setIsFollower(true);
+  };
 
-	const devContributions = () => {
-		const result = [];
-		documents.map((document) => {
-			if (document.user_id === jwt.userId) {
-				result.push(document);
-			}
-		});
+  const devContributions = () => {
+    const result = [];
+    documents.map(document => {
+      if (document.user_id === jwt.userId) {
+        result.push(document);
+      }
+    });
 
-		return result;
-	};
+    return result;
+  };
 
-	if (
-		project !== undefined &&
-		documents !== undefined &&
-		usersFollowing !== undefined &&
-		myContributions !== undefined &&
-		typeOfProfile !== undefined
-	) {
-		return (
-			<section className='container'>
-				<Header isAccessWindow={false} />
-				<section className='project'>
-					<ul>
-						<Link to={`/user/${project.user_id}`}>
-							<li>
-								<div id='small-icon' className='profile-photo'>
-									<img src={project.user_avatar_url} alt=''></img>
-								</div>
-								<button id='org-name'>{project.user_name}</button>
-							</li>
-						</Link>
-						<li>
-							{typeOfProfile === OWNER_VIEW && <OrgProject project={project} />}
-							{typeOfProfile !== OWNER_VIEW && <DevProject project={project} />}
-						</li>
-						<li>
-							{typeOfProfile === DEVELOPER_VIEW && (
-								<form>
-									<fieldset>
-										<legend>Tus contribuciones</legend>
-										<section className='contributions'>
-											{myContributions.map((document, index) => (
-												<section id='contrib-row'>
-													<a href={document.file_url}>{document.title}</a>
-													<button>eliminar</button>
-												</section>
-											))}
-										</section>
-										<label for='contributions'>Sube una propuesta</label>
-										<input
-											type='file'
-											id='contributions'
-											name='contributions'
-											accept='pdf'
-											ref={register}
-										></input>
-									</fieldset>
-								</form>
-							)}
-							{typeOfProfile === OWNER_VIEW && (
-								<section>
-									<p>Contribuciones</p>
-									<section className='contributions'>
-										{documents.map((document, index) => (
-											<div id='contrib-row'>
-												<Link to={`/user/${document.user_id}`}>
-													<button>
-														<div id='small-icon' className='profile-photo'>
-															<img src={document.user_avatar_url} alt=''></img>
-														</div>
-														<p>{document.user_name}</p>
-													</button>
-												</Link>
-												<a href={document.file_url}>{document.title}</a>
-												<div>{document.rating} estrellas</div>
-											</div>
-										))}
-									</section>
-								</section>
-							)}
-						</li>
-						<li>
-							<p>Seguidores de este proyecto</p>
-							<section className='followers'>
-								{usersFollowing.map((user, index) => (
-									<Link to={`/user/${user.user_id}`} key={index}>
-										<button>
-											<div id='small-icon' className='profile-photo'>
-												<img src={user.avatar_url} alt='' />
-											</div>
-											<p>{user.name}</p>
-										</button>
-									</Link>
-								))}
-							</section>
-						</li>
-						<li id='bottom'>
-							{typeOfProfile === DEVELOPER_VIEW && (
-								<button onClick={handleFollow}>
-									{isFollower ? "Dejar de seguir proyecto" : "Seguir proyecto"}
-								</button>
-							)}
-							{typeOfProfile === OWNER_VIEW && <button>Cerrar proyecto</button>}
-						</li>
-					</ul>
-				</section>
-			</section>
-		);
-	} else {
-		return <p>Cargando proyecto...</p>;
-	}
+  if (
+    project !== undefined &&
+    documents !== undefined &&
+    usersFollowing !== undefined &&
+    myContributions !== undefined &&
+    typeOfProfile !== undefined
+  ) {
+    return (
+      <section className="container">
+        <Header isAccessWindow={false} />
+        <section className="project">
+          <ul className="project-view">
+            <Link to={`/user/${project.user_id}`}>
+              <li>
+                <div id="small-icon" className="profile-photo">
+                  <img src={project.user_avatar_url} alt=""></img>
+                </div>
+                <button id="org-name">{project.user_name}</button>
+              </li>
+            </Link>
+            <li>
+              {typeOfProfile === OWNER_VIEW && <OrgProject project={project} />}
+              {typeOfProfile !== OWNER_VIEW && <DevProject project={project} />}
+            </li>
+            <li>
+              {typeOfProfile === DEVELOPER_VIEW && (
+                <form>
+                  <fieldset>
+                    <legend>Tus contribuciones</legend>
+                    <section className="contributions">
+                      {myContributions.map((document, index) => (
+                        <section id="contrib-row">
+                          <a href={document.file_url}>{document.title}</a>
+                          <button>eliminar</button>
+                        </section>
+                      ))}
+                    </section>
+                    <label for="contributions" id="document">
+                      Seleccionar archivo
+                    </label>
+                    <input
+                      type="file"
+                      id="contributions"
+                      name="contributions"
+                      accept="pdf"
+                      ref={register}
+                    ></input>
+                  </fieldset>
+                </form>
+              )}
+              {typeOfProfile === OWNER_VIEW && (
+                <section>
+                  <p>Contribuciones</p>
+                  <section className="contributions">
+                    {documents.map((document, index) => (
+                      <div id="contrib-row">
+                        <Link to={`/user/${document.user_id}`}>
+                          <button>
+                            <div id="small-icon" className="profile-photo">
+                              <img src={document.user_avatar_url} alt=""></img>
+                            </div>
+                            <p>{document.user_name}</p>
+                          </button>
+                        </Link>
+                        <a href={document.file_url}>{document.title}</a>
+                        <div>{document.rating} estrellas</div>
+                      </div>
+                    ))}
+                  </section>
+                </section>
+              )}
+            </li>
+            <li>
+              <p>Seguidores de este proyecto</p>
+              <section className="followers">
+                {usersFollowing.map((user, index) => (
+                  <Link to={`/user/${user.user_id}`} key={index}>
+                    <button>
+                      <div id="small-icon" className="profile-photo">
+                        <img src={user.avatar_url} alt="" />
+                      </div>
+                      <p>{user.name}</p>
+                    </button>
+                  </Link>
+                ))}
+              </section>
+            </li>
+            <li id="bottom">
+              {typeOfProfile === DEVELOPER_VIEW && (
+                <button onClick={handleFollow}>
+                  {isFollower ? "Dejar de seguir proyecto" : "Seguir proyecto"}
+                </button>
+              )}
+              {typeOfProfile === OWNER_VIEW && <button>Cerrar proyecto</button>}
+            </li>
+          </ul>
+        </section>
+      </section>
+    );
+  } else {
+    return <p>Cargando proyecto...</p>;
+  }
 }
