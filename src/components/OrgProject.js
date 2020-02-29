78df9d1bc8d1ec39ecad0cb9f7e19f1ada223396
Alberto <alberto.gespi@gmail.com>
2020-02-29 22:46:06 +0100

    AGE

--- src/components/OrgProject.js
+++ src/components/OrgProject.js
@@ -4,123 +4,137 @@ import { updateProject } from "../http/projectService";
 import { convertISOtoDate } from "../functions/convertISOtoDate";
 
 export function OrgProject({ project }) {
-	const categories = [
-		"Blog",
-		"e-Commerce",
-		"e-Learning",
-		"Corporativa",
-		"Noticias",
-		"Wikis",
-	];
+  const categories = [
+    "Blog",
+    "e-Commerce",
+    "e-Learning",
+    "Corporativa",
+    "Noticias",
+    "Wikis"
+  ];
 
-	const complexities = ["Fácil", "Medio", "Difícil"];
+  const complexities = ["Fácil", "Medio", "Difícil"];
 
-	const { register, formState, handleSubmit } = useForm({
-		mode: "onBlur",
-	});
+  const { register, formState, handleSubmit } = useForm({
+    mode: "onBlur"
+  });
 
-	const handleUpdateProject = (formData) => {
-		updateProject(formData, project.project_id);
-	};
+  const handleUpdateProject = formData => {
+    updateProject(formData, project.project_id);
+  };
 
-	return (
-		<form onSubmit={handleSubmit(handleUpdateProject)}>
-			<section id='top'>
-				<fieldset id='visible-info'>
-					<ul>
-						<li>
-							<label for='title'>Título</label>
-							<input
-								type='text'
-								id='title'
-								name='title'
-								defaultValue={project.title}
-								ref={register({
-									required: "El título es obligatorio",
-								})}
-							></input>
-						</li>
-						<li>
-							<label for='description'>Resumen</label>
-							<textarea
-								id='description'
-								name='description'
-								defaultValue={project.description}
-								ref={register({
-									required: "El resumen es obligatorio",
-								})}
-							></textarea>
-						</li>
-					</ul>
-				</fieldset>
-				<section id='project-info'>
-					<ul>
-						<li>
-							<p>Creado el {convertISOtoDate(project.created_at)}</p>
-						</li>
-						<li>
-							<p>
-								{project.number_of_followers}
-								{project.number_of_followers === 1 ? " seguidor" : " seguidores"}
-							</p>
-						</li>
-						<li id='selects'>
-							<label for='category'>Categoría</label>
-							<select
-								className='select-css'
-								name='category'
-								id='category'
-								ref={register({
-									required: "La categoría es obligatoria",
-								})}
-							>
-								<option>Selecciona...</option>
-								{categories.map((category, index) => (
-									<option selected={project.category === category} value={category}>
-										{category}
-									</option>
-								))}
-							</select>
-						</li>
-						<li id='selects'>
-							<label for='complexity'>Complejidad</label>
-							<select
-								className='select-css'
-								name='complexity'
-								id='complexity'
-								ref={register({
-									required: "La complejidad es obligatoria",
-								})}
-							>
-								<option>Selecciona...</option>
-								{complexities.map((complexity, index) => (
-									<option selected={project.complexity === index + 1} value={index + 1}>
-										{complexity}
-									</option>
-								))}
-							</select>
-						</li>
-					</ul>
-				</section>
-			</section>
-			<section id='middle'>
-				<fieldset>
-					<label for='details'>Descripción detallada</label>
-					<textarea
-						id='details'
-						name='details'
-						defaultValue={project.details}
-						ref={register({
-							required: "La descripción es obligatoria",
-						})}
-					></textarea>
-				</fieldset>
-			</section>
-			<section id='bottom'>
-				<button className='form' type='submit' disabled={formState.isSubmitting}>
-					Guardar cambios
-				</button>
-			</section>
-		</form>
-	);
+  return (
+    <form id="org-view-project" onSubmit={handleSubmit(handleUpdateProject)}>
+      <section id="top">
+        <fieldset id="visible-info">
+          <ul>
+            <li>
+              <label for="title">Título</label>
+              <input
+                type="text"
+                id="title"
+                name="title"
+                defaultValue={project.title}
+                ref={register({
+                  required: "El título es obligatorio"
+                })}
+              ></input>
+            </li>
+            <li>
+              <label for="description">Resumen</label>
+              <textarea
+                id="description"
+                name="description"
+                defaultValue={project.description}
+                ref={register({
+                  required: "El resumen es obligatorio"
+                })}
+              ></textarea>
+            </li>
+          </ul>
+        </fieldset>
+        <section id="project-info">
+          <ul>
+            <li>
+              <p>Creado el {convertISOtoDate(project.created_at)}</p>
+            </li>
+            <li>
+              <p>
+                {project.number_of_followers}
+                {project.number_of_followers === 1
+                  ? " seguidor"
+                  : " seguidores"}
+              </p>
+            </li>
+            <li id="selects">
+              <label for="category">Categoría</label>
+              <select
+                className="select-css"
+                name="category"
+                id="category"
+                ref={register({
+                  required: "La categoría es obligatoria"
+                })}
+              >
+                <option>Selecciona...</option>
+                {categories.map((category, index) => (
+                  <option
+                    selected={project.category === category}
+                    value={category}
+                  >
+                    {category}
+                  </option>
+                ))}
+              </select>
+            </li>
+            <li id="selects">
+              <label for="complexity">Complejidad</label>
+              <select
+                className="select-css"
+                name="complexity"
+                id="complexity"
+                ref={register({
+                  required: "La complejidad es obligatoria"
+                })}
+              >
+                <option>Selecciona...</option>
+                {complexities.map((complexity, index) => (
+                  <option
+                    selected={project.complexity === index + 1}
+                    value={index + 1}
+                  >
+                    {complexity}
+                  </option>
+                ))}
+              </select>
+            </li>
+          </ul>
+        </section>
+      </section>
+      <section id="middle">
+        <fieldset>
+          <label for="details" id="details-label">
+            Descripción detallada
+          </label>
+          <textarea
+            id="details"
+            name="details"
+            defaultValue={project.details}
+            ref={register({
+              required: "La descripción es obligatoria"
+            })}
+          ></textarea>
+        </fieldset>
+      </section>
+      <section id="bottom">
+        <button
+          className="form"
+          type="submit"
+          disabled={formState.isSubmitting}
+        >
+          Guardar cambios
+        </button>
+      </section>
+    </form>
+  );
 }
--- src/pages/User.js
+++ src/pages/User.js
@@ -8,165 +8,176 @@ import { getUser } from "../http/userService";
 import { MyProjectsOrg } from "../components/MyProjectsOrg";
 import { MyProjectsDev } from "../components/MyProjectsDev";
 import {
-	getOrgProjects,
-	getFollowedProjects,
-	getContributedProjects,
-	getNumberOfContributions,
+  getOrgProjects,
+  getFollowedProjects,
+  getContributedProjects,
+  getNumberOfContributions
 } from "../http/projectsService";
 import { Footer } from "../components/Footer";
 
 export function User() {
-	const categories = [
-		"Todas",
-		"Blog",
-		"Corporativa",
-		"e-Commerce",
-		"e-Learning",
-		"Noticias",
-		"Wikis",
-	];
+  const categories = [
+    "Todas",
+    "Blog",
+    "Corporativa",
+    "e-Commerce",
+    "e-Learning",
+    "Noticias",
+    "Wikis"
+  ];
 
-	const complexities = ["Todas", "Fácil", "Medio", "Difícil"];
+  const complexities = ["Todas", "Fácil", "Medio", "Difícil"];
 
-	const userId = window.location.href.split("/")[4];
-	const [user, setUser] = useState(undefined);
-	const [isOrgProfile, setIsOrgProfile] = useState(undefined);
+  const userId = window.location.href.split("/")[4];
+  const [user, setUser] = useState(undefined);
+  const [isOrgProfile, setIsOrgProfile] = useState(undefined);
 
-	//IsOrgProfile = true
-	const [orgProjects, setOrgProjects] = useState([]);
+  //IsOrgProfile = true
+  const [orgProjects, setOrgProjects] = useState([]);
 
-	//IsOrgProfile = false
-	const [followedProjects, setFollowedProjects] = useState([]);
-	const [contributedProjects, setContributedProjects] = useState([]);
-	const [rating, setRating] = useState(undefined);
-	const [numberOfContributions, setNumberOfContributions] = useState(0);
+  //IsOrgProfile = false
+  const [followedProjects, setFollowedProjects] = useState([]);
+  const [contributedProjects, setContributedProjects] = useState([]);
+  const [rating, setRating] = useState(undefined);
+  const [numberOfContributions, setNumberOfContributions] = useState(0);
 
-	const [buttonSelected, setButtonSelected] = useState(true);
+  const [buttonSelected, setButtonSelected] = useState(true);
 
-	//filters
-	const [filtersState, setFiltersState] = useState({
-		category: '',
-		complexity: '',
-	  });
+  //filters
+  const [filtersState, setFiltersState] = useState({
+    category: "",
+    complexity: ""
+  });
 
-	const history = useHistory();
+  const history = useHistory();
 
-	let historyQuery = "";
-	const updateQuery = () => {
-		console.log(filtersState);
-		  if (filtersState.category !== '') {
-			historyQuery += `/?category=${filtersState.category}`;
-		  }
-	  
-		  if (filtersState.complexity !== '') {
-			if (historyQuery === "") {
-			  historyQuery += `/?complexity=${filtersState.complexity}`;
-			} else {
-			  historyQuery += `&complexity=${filtersState.complexity}`;
-			}
-		  }
-	  
-		  history.push(`/user/${userId}` + historyQuery);
-		};
+  let historyQuery = "";
+  const updateQuery = () => {
+    console.log(filtersState);
+    if (filtersState.category !== "") {
+      historyQuery += `/?category=${filtersState.category}`;
+    }
 
-	useEffect(() => {
-		getUser(userId).then((response) => {
-			setUser(response.data);
-			setIsOrgProfile(response.data.role === "org");
-		});
-		getAvgRatings(userId).then((response) => {
-			const ratingRounded = Math.round(response.data * 2) / 2;
-			setRating(ratingRounded);
-		});
-		getNumberOfContributions(userId).then((response) => {
-			setNumberOfContributions(response.data);
-		});
-		updateQuery();
-		if (isOrgProfile) {
-			getOrgProjects(userId, historyQuery).then((response) => {
-				setOrgProjects(response.data);
-			});
-		} else {
-			getFollowedProjects(userId, historyQuery).then((response) =>
-				setFollowedProjects(response.data),
-			);
-			getContributedProjects(userId, historyQuery).then((response) =>
-				setContributedProjects(response.data),
-			);
-		}
-	}, [isOrgProfile, filtersState, buttonSelected]);
+    if (filtersState.complexity !== "") {
+      if (historyQuery === "") {
+        historyQuery += `/?complexity=${filtersState.complexity}`;
+      } else {
+        historyQuery += `&complexity=${filtersState.complexity}`;
+      }
+    }
 
-	if (
-		user !== undefined &&
-		rating !== undefined &&
-		orgProjects !== undefined &&
-		followedProjects !== undefined &&
-		contributedProjects !== undefined &&
-		numberOfContributions !== undefined
-	) {
-		console.log(userId);
-		return (
-			<section className='container'>
-				<Header isAccessWindow={false} />
-				<section className='body'>
-					<div className='header-user'>
-						<section className='centered-container' id='user-container'>
-							<p id='profile-role'>
-								{user.role === "dev" ? "Perfil Desarrollador" : "Perfil Organización"}
-							</p>
-							<div className='profile-photo' id='big-icon'>
-								<img
-									src={user.avatarUrl || require("../images/default-avatar.jpg")}
-									alt=''
-									name='profile photo'
-								></img>
-							</div>
-							<h1>{user.name}</h1>
+    history.push(`/user/${userId}` + historyQuery);
+  };
 
-							<div className='centered-container' id='rating'>
-								{user.role === "dev" && (
-									<div className='centered-container'>
-										<SimpleRating readOnly={true} value={rating} id='stars' />
-										<p>Puntuación media</p>
-										{rating
-											? `${rating} ${
-													rating === 1 ? "estrella" : "estrellas"
-											  } / ${numberOfContributions} ${
-													numberOfContributions === 1 ? "contribución" : "contribuciones"
-											  }`
-											: "Este usuario aún no ha recibido puntuaciones"}
-									</div>
-								)}
-							</div>
-							<div className='centered-container' id='contact'>
-								<h2>Contacto</h2>
-								<p>{user.contactEmail || user.email}</p>
-								{user.contactWeb !== "NULL" && <a href={user.contactWeb}>Enlace web</a>}
-							</div>
-						</section>
-					</div>
-					<section className='projects'>
-					<Filters filtersState={filtersState} setFiltersState={setFiltersState}/>
-						{isOrgProfile ? (
-							<MyProjectsOrg
-								projects={orgProjects}
-								buttonSelected={buttonSelected}
-								setButtonSelected={setButtonSelected}
-							/>
-						) : (
-							<MyProjectsDev
-								followedProjects={followedProjects}
-								contributedProjects={contributedProjects}
-								buttonSelected={buttonSelected}
-								setButtonSelected={setButtonSelected}
-							/>
-						)}
-					</section>
-				</section>
-				<Footer />
-			</section>
-		);
-	} else {
-		return <p>Cargando datos de usuario...</p>;
-	}
+  useEffect(() => {
+    getUser(userId).then(response => {
+      setUser(response.data);
+      setIsOrgProfile(response.data.role === "org");
+    });
+    getAvgRatings(userId).then(response => {
+      const ratingRounded = Math.round(response.data * 2) / 2;
+      setRating(ratingRounded);
+    });
+    getNumberOfContributions(userId).then(response => {
+      setNumberOfContributions(response.data);
+    });
+    updateQuery();
+    if (isOrgProfile) {
+      getOrgProjects(userId, historyQuery).then(response => {
+        setOrgProjects(response.data);
+      });
+    } else {
+      getFollowedProjects(userId, historyQuery).then(response =>
+        setFollowedProjects(response.data)
+      );
+      getContributedProjects(userId, historyQuery).then(response =>
+        setContributedProjects(response.data)
+      );
+    }
+  }, [isOrgProfile, filtersState, buttonSelected]);
+
+  if (
+    user !== undefined &&
+    rating !== undefined &&
+    orgProjects !== undefined &&
+    followedProjects !== undefined &&
+    contributedProjects !== undefined &&
+    numberOfContributions !== undefined
+  ) {
+    console.log(userId);
+    return (
+      <section className="container">
+        <Header isAccessWindow={false} />
+        <section className="body">
+          <div className="header-user">
+            <section className="centered-container" id="user-container">
+              <p id="profile-role">
+                {user.role === "dev"
+                  ? "Perfil Desarrollador"
+                  : "Perfil Organización"}
+              </p>
+              <div className="profile-photo" id="big-icon">
+                <img
+                  src={
+                    user.avatarUrl || require("../images/default-avatar.jpg")
+                  }
+                  alt=""
+                  name="profile photo"
+                ></img>
+              </div>
+              <h1>{user.name}</h1>
+
+              <div className="centered-container" id="rating">
+                {user.role === "dev" && (
+                  <div className="centered-container">
+                    <SimpleRating readOnly={true} value={rating} id="stars" />
+                    <p>Puntuación media</p>
+                    {rating
+                      ? `${rating} ${
+                          rating === 1 ? "estrella" : "estrellas"
+                        } / ${numberOfContributions} ${
+                          numberOfContributions === 1
+                            ? "contribución"
+                            : "contribuciones"
+                        }`
+                      : "Este usuario aún no ha recibido puntuaciones"}
+                  </div>
+                )}
+              </div>
+              <div className="centered-container" id="contact">
+                <h2>Contacto</h2>
+                <p>{user.contactEmail || user.email}</p>
+                {user.contactWeb !== "NULL" && (
+                  <a href={user.contactWeb}>Sitio web</a>
+                )}
+              </div>
+            </section>
+          </div>
+          <section className="projects">
+            <Filters
+              filtersState={filtersState}
+              setFiltersState={setFiltersState}
+            />
+            {isOrgProfile ? (
+              <MyProjectsOrg
+                projects={orgProjects}
+                buttonSelected={buttonSelected}
+                setButtonSelected={setButtonSelected}
+              />
+            ) : (
+              <MyProjectsDev
+                followedProjects={followedProjects}
+                contributedProjects={contributedProjects}
+                buttonSelected={buttonSelected}
+                setButtonSelected={setButtonSelected}
+              />
+            )}
+          </section>
+        </section>
+        <Footer />
+      </section>
+    );
+  } else {
+    return <p>Cargando datos de usuario...</p>;
+  }
 }
